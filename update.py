903869e045f0f488aa096e78c6b4fabf80f0b795
JOCKROM89 <78678574+JOCKROM89@users.noreply.github.com>
2025-09-16 09:14:03 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,21 +1,30 @@
 import pymysql
 import pandas as pd
 
-db =pymysql.connect(host='localhost',
-                     user='root',
-                     password='root',
-                     database='user')
-cursor = db.cursor()
+
 sql="update xiaoqing2024 join(select id from xiaoqing2024 where NO = '{}') as sub ON xiaoqing2024.id = sub.id SET xiaoqing2024.price = {};"
 # sql_="insert into xiaoqing2024(NO,NAME,speces,brand,unit,price) values(%s,%s,%s,%s,%s,%s)"
 sql_="insert into xiaoqing2024(NO,NAME,speces,brand,unit,price) select %s,%s,%s,%s,%s,%s where not exists (select 1 from xiaoqing2024 where NO = %s and price =%s);"
 sql_1 = "select NO,NAME,speces,brand,unit,price from xiaoqing2024;"
 
 
+def connect_db():
+    try:
+        db =pymysql.connect(host='localhost',
+                    user='root',
+                    password='root',
+                    database='user')
+        cursor = db.cursor()
+        return db,cursor
+    except pymysql.MySQLError as e:
+        return None, None
 
 def update(file1):
+    db, cursor = connect_db()  # 每次更新时重新连接数据库
+    if db is None or cursor is None:
+        return '数据库连接失败'
     try:
-        df_ = pd.read_excel(file1, usecols='C:H',skiprows=0 ,names=None)
+        df_ = pd.read_excel(file1, usecols='C:H',skiprows=1 ,names=None)
         # df_replaced = df.replace(to_replace=pd.NA, value='NULL')
         df_replaced_=df_.fillna('NULL')
         df_li_ = df_replaced_.values.tolist()
@@ -32,25 +41,22 @@ def update(file1):
             mysql_NO.append(a[0])
 
 
-
         for i in df_li_:
             i_tuple = tuple(i)
             if i not in mysql_data:
-                if i[0] not in mysql_NO: 
+                if i[0] not in mysql_NO and i[0] != 'NULL': 
                     
                     i_tuple=i_tuple+(i_tuple[0],i_tuple[5])
                                       
                     cursor.execute(sql_,i_tuple)
                 
                     db.commit()
-                    # print(i_tuple)
                 else:                   
-                    # print(sql.format(i_tuple[0],i_tuple[5]))
                     cursor.execute(sql.format(i_tuple[0],i_tuple[5]))
                     db.commit()
-            else:
-                return
-        return
+            else:               
+                return 
+        return 'update is over'
     except Exception as e:
         print(f"An error occurred: {e}")
         db.rollback()
@@ -59,8 +65,8 @@ def update(file1):
         db.close()
 
 
-# file1 = 'D:/2024-11月数据库（降价后）.xlsx'
+
+
+
+# file1 = 'D:/2025-9月数据库.xlsx'
 # update(file1)
-
-
-
